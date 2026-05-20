f1ad8beababa292ba372d7fcaba820c0ebae805c
rajalingarao4545 <rajalingarao4545@gmail.com>
2026-05-20 23:48:34 +0530

    amazon linux create ec2

--- provider.tf
+++ provider.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 6.37.0"
     }
   }
 backend "s3" {
     bucket = "linga-devops-rs"
     key    = "amazon-linux-key"
     region = "us-east-1"
-    dynamodb_table = "linga-devops-locking"
-    #use_lockfile = true
+    #dynamodb_table = "roboshop13-locking"
+    use_lockfile = true
     }
   }
 provider "aws" {
